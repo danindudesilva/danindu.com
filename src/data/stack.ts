e14153db4fc9b19cacc20bb38adc3652927696ed
Danindu de Silva <danindu001@gmail.com>
2026-03-19 22:48:34 +0000

    data: add tech stack

---
+++ src/data/stack.ts
@@ -0,0 +1,48 @@
+export type StackLogo = {
+  name: string;
+  src: string;
+  alt: string;
+};
+
+export const coreStack: StackLogo[] = [
+  {
+    name: "Go",
+    src: "/logos/go.svg",
+    alt: "Go logo",
+  },
+  {
+    name: "TypeScript",
+    src: "/logos/typescript.svg",
+    alt: "TypeScript logo",
+  },
+  {
+    name: "Node.js",
+    src: "/logos/nodejs.svg",
+    alt: "Node.js logo",
+  },
+  {
+    name: "PostgreSQL",
+    src: "/logos/postgresql.svg",
+    alt: "PostgreSQL logo",
+  },
+  {
+    name: "Docker",
+    src: "/logos/docker.svg",
+    alt: "Docker logo",
+  },
+  {
+    name: "Kubernetes",
+    src: "/logos/kubernetes.svg",
+    alt: "Kubernetes logo",
+  },
+  {
+    name: "GCP",
+    src: "/logos/gcp.svg",
+    alt: "Google Cloud logo",
+  },
+  {
+    name: "AWS",
+    src: "/logos/aws.svg",
+    alt: "AWS logo",
+  },
+];
